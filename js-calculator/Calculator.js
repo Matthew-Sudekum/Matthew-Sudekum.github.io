e1a75686929b6deb1e2e28ae96ea50d2b1647482
Matthew-Sudekum <69998798+Matthew-Sudekum@users.noreply.github.com>
2021-09-13 22:06:41 -0400

    Added js-calculator project

---
+++ js-calculator/Calculator.js
@@ -0,0 +1,25 @@
+function display(val){
+    if(document.getElementById('display').getElementsByTagName('p')[0].innerHTML == '0')
+        document.getElementById('display').getElementsByTagName('p')[0].innerHTML = val;
+    else
+        document.getElementById('display').getElementsByTagName('p')[0].innerHTML += val;
+}
+
+function clr(){
+    document.getElementById('display').getElementsByTagName('p')[0].innerHTML = '0';
+}
+
+function del(){
+    let str = document.getElementById('display').getElementsByTagName('p')[0].innerHTML;
+    let editedStr = str.slice(0, -1);
+    if(editedStr == '')
+        editedStr = '0';
+    document.getElementById('display').getElementsByTagName('p')[0].innerHTML = editedStr;
+}
+
+function equals(){
+    let str = document.getElementById('display').getElementsByTagName('p')[0].innerHTML;
+    let evalStr = eval(str);
+
+    document.getElementById('display').getElementsByTagName('p')[0].innerHTML = evalStr;
+}
