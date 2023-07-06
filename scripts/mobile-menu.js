5bb01c7c41e52dc0a39c2631e901e37c6414caaa
Matthew Sudekum <matthew.sudekum@outlook.com>
2023-07-06 00:06:34 -0400

    Added mobile menu to all pages; Disabled background interaction when menu is visible

--- scripts/mobile-menu.js
+++ scripts/mobile-menu.js
@@ -1,7 +1,11 @@
+const html = document.querySelector('html');
 const checkbox = document.querySelector('input[type="checkbox"]');
 checkbox.addEventListener('change', function () {
     const mobileElements = document.querySelectorAll('.menu');
     mobileElements.forEach(function (element) {
+        if(element.id=='menu-underlay')
+            element.style.display = checkbox.checked ? 'block' : 'none';
         element.style.opacity = checkbox.checked ? 1 : 0;
     });
+    html.classList.toggle('disable-scroll');
 });
